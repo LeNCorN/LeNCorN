7e7f77992bd9881e05ab785bac93aff3c335ff80
Ilya Shaytor <shaytor04@gmail.com>
2022-12-07 07:11:02 +0200

    Шайтор

--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -1,20 +1,42 @@
+from string import ascii_lowercase, ascii_uppercase
+
+
 def encrypt_vigenere(plaintext: str, keyword: str) -> str:
-    keyword_length = len(keyword)
-    keyword_as_int = [ord(i) for i in keyword]
-    plaintext_int = [ord(i) for i in plaintext]
-    ciphertext = ''
-    for i in range(len(plaintext_int)):
-        value = (plaintext_int[i] + keyword_as_int[i % keyword_length]) % 26
-        ciphertext += chr(value + 65)
+    ciphertext = ""
+    key_dict = {symbol: i for i, symbol in enumerate(ascii_uppercase)}
+
+    for i, symbol in enumerate(plaintext):
+        alphabet = ascii_uppercase if symbol.isupper() else ascii_lowercase
+
+        symbol_index_in_ascii = alphabet.find(symbol)
+        if symbol_index_in_ascii == -1:
+            ciphertext += symbol
+            continue
+
+        key_for_symbol = keyword[i] if i < len(keyword) else keyword[i % len(keyword)]
+        shift = key_dict[key_for_symbol.upper()]
+        try:
+            ciphertext += alphabet[symbol_index_in_ascii + shift]
+        except IndexError:
+            ciphertext += alphabet[(symbol_index_in_ascii + shift) % len(alphabet)]
+
     return ciphertext
 
 
 def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
-    keyword_length = len(keyword)
-    keyword_as_int = [ord(i) for i in keyword]
-    ciphertext_int = [ord(i) for i in ciphertext]
-    plaintext = ''
-    for i in range(len(ciphertext_int)):
-        value = (ciphertext_int[i] - keyword_as_int[i % keyword_length]) % 26
-        plaintext += chr(value + 65)
+    plaintext = ""
+    key_dict = {symbol: i for i, symbol in enumerate(ascii_uppercase)}
+
+    for i, symbol in enumerate(ciphertext):
+        alphabet = ascii_uppercase if symbol.isupper() else ascii_lowercase
+
+        symbol_index_in_ascii = alphabet.find(symbol)
+        if symbol_index_in_ascii == -1:
+            plaintext += symbol
+            continue
+
+        key_for_symbol = keyword[i] if i < len(keyword) else keyword[i % len(keyword)]
+        shift = key_dict[key_for_symbol.upper()]
+        plaintext += alphabet[symbol_index_in_ascii - shift]
+
     return plaintext
